201e8e8e6f500c7ae76e14215d1f202b627f0f57
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-02-21 14:51:50 +0300

    home_row

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,6 +18,18 @@ const Home = () => {
             price={9499.0}
             image="https://m.media-amazon.com/images/I/41wddqDWz6L._MCnd_AC_.jpg"
           />
+          <Product
+            id="5646"
+            title="MacBook Pro"
+            price={14999.0}
+            image="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-14-digitalmat-gallery-1-202310?wid=728&hei=666&fmt=png-alpha&.v=1696971522628"
+          />
+          <Product
+            id="1455"
+            title="iPad Air"
+            price={14999.0}
+            image="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipadair-digitalmat-gallery-1-202203?wid=728&hei=666&fmt=png-alpha&.v=1644962768135"
+          />
         </div>
       </div>
     </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -10,11 +10,10 @@ const Product = ({ id, title, image, price }) => {
     dispatch({
       type: "ADD_TO_BASKET",
       item: {
-        id: 1,
-        title: "Product Title",
-        image:
-          "https://images-eu.ssl-images-amazon.com/images/G/41/TR-hq/2023/img/Prime/SIGN_UP_BOUNCEBACK_PROMO/TR23_PRIME_SIGN_UP_BOUNCEBACK_PROMO_Desktop_Card_758x608._SY608_CB574268667_.jpg",
-        price: 10,
+        id: id,
+        title: title,
+        image: image,
+        price: price,
       },
     });
   };
